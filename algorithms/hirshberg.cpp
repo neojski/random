598231f1ed23b98e840dd0dd8ca189ebb945ff33
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-10-14 20:16:28 +0200

    Shorten hirshberg algorithm

--- algorithms/hirshberg.cpp
+++ algorithms/hirshberg.cpp
@@ -23,30 +23,22 @@ string rev(const string& a){
 // here we should have had references
 string lcs(string a, string b){
   if(a.size() > b.size()) swap(a,b);
-  if(a.size() > 1){
-    int c = b.size()/2;
-    vector<int> v1 = getrow(a, b.substr(0, c));
-    vector<int> v2 = getrow(rev(a), rev(b.substr(c, b.size()-c)));
+  int c = b.size()/2;
+  vector<int> v1 = getrow(a, b.substr(0, c));
+  vector<int> v2 = getrow(rev(a), rev(b.substr(c, b.size()-c)));
 
-    int max = 0;
-    int j;
-    for(int i = 0; i < v1.size(); i++){
-      int sum = v1[i] + v2[v2.size() - 1 - i];
-      if(sum >= max){
-        max = sum;
-        j = i;
-      }
+  int max = 0, j;
+  for(int i = 0; i < v1.size(); i++){
+    int sum = v1[i] + v2[v2.size() - 1 - i];
+    if(sum >= max){
+      max = sum;
+      j = i;
     }
-    return lcs(a.substr(0, j), b.substr(0, c)) +
-      lcs(a.substr(j, a.size() - j), b.substr(c, b.size() - c));
-  }else{
-    if(b.find(a) != string::npos){
-      return a;
-    }
-    return "";
   }
+  if(a.size() <= 1) return max > 0 ? a : "";
+  return lcs(a.substr(0, j), b.substr(0, c)) +
+    lcs(a.substr(j, a.size() - j), b.substr(c, b.size() - c));
 }
-
 int main() {
   int z; cin >> z;
   while(z--){
