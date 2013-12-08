779e2b1acf06e4905d82bfcbfafe930fade55dee
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-12-08 15:20:01 +0100

    Add eps, fix convergence condition (at least one must be ok)

--- numeryczne/gauss-seidel.m
+++ numeryczne/gauss-seidel.m
@@ -1,38 +1,38 @@
 % check whether matrix is strictly diagonally dominant
 function res = isStrictDiagonallyDominant(A)
-	res = all( 2*abs(diag(A)) > sum(abs(A),2) )
+  res = all( 2*abs(diag(A)) > sum(abs(A),2) )
 endfunction
 
 % check whether matrix is positive definite
 function res = isPositiveDefinite(A)
-	res = all(eig(A) > 0)
+  res = all(eig(A) > 0)
 endfunction
 
 % check whether matrix is symmetric
 function res = isSymmetric(A)
-	res = all(all(A == A'))
+  res = all(all(A == A'))
 endfunction
 
 % check whether matrix is symmetric and positive definite
 function res = isSymmetricPositiveDefinite(A)
-	res = isPositiveDefinite(A) && isSymmetric(A)
+  res = isPositiveDefinite(A) && isSymmetric(A)
 endfunction
 
 % gauss seidel method
-function  x = gaussSeidel(A, b)
-	if !isStrictDiagonallyDominant(A) || !isSymmetricPositiveDefinite(A)
-		printf("%s\n", "Method can be non-convergent!")
-	endif
+function  x = gaussSeidel(A, b, eps)
+  if !isStrictDiagonallyDominant(A) && !isSymmetricPositiveDefinite(A)
+    printf("%s\n", "Method can be non-convergent!")
+  endif
 
-	L = tril(A)
-	Us = triu(A,1)
-	invL = inv(L)
+  L = tril(A)
+  Us = triu(A,1)
+  invL = inv(L)
 
-	x = [1; 0; 0]
+  x = [1; 0; 0]
 
-	for i = 1:100
-		x = invL * (b - Us * x)
-	endfor
+  while norm(A*x - b) > eps
+    x = invL * (b - Us * x)
+  end
 endfunction
 
 
@@ -40,4 +40,4 @@ endfunction
 A = [2 0 1; 0 1 0; 0 0 1]
 b = [1; 2; 3]
 
-x = gaussSeidel(A,b)
+x = gaussSeidel(A, b, 1e-10)
