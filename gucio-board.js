e0cf951279e1d49928066fbd179a8d2099b289e7
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2014-04-03 16:53:36 +0200

    Add script to make complex shapes on gucio's board

---
+++ gucio-board.js
@@ -0,0 +1,30 @@
+var request = require('request');
+
+
+function wielokat(n, maxK){
+
+  var z = 0;
+  for (var k = .01; k < maxK; k+=.03){
+    for(var i = 0; i < n; i++) {
+      var str = '<?xml version="1.0"?> <newpath userId="' + (++z) + '">';
+      str += '<point x="' + ((1-k)/2+k*(Math.cos(2*Math.PI*(i/n))+1)/2) + '" y="' + ((1-k)/2+k*((Math.sin( 2*Math.PI * (i/n))+1)/2)) + '"/>';
+      ++i;
+      str += '<point x="' + ((1-k)/2+k*(Math.cos(2*Math.PI*(i/n))+1)/2) + '" y="' + ((1-k)/2+k*((Math.sin( 2*Math.PI * (i/n))+1)/2)) + '"/>';
+      str += '</newpath>';
+      --i;
+
+      request.post({
+        headers: {'content-type' :'application/xml'},
+        url: 'http://grzegorz_gutowski.staff.tcs.uj.edu.pl/board/newpath/',
+        body: str
+      }, function (error, response, body) {
+        console.log(error);
+        console.log(body);
+      });
+    }
+  }
+
+}
+
+wielokat(3, 1);
+wielokat(6, .3);
