80e7d9cade3fe0b238ce242c87772d273629ccac
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2017-02-14 21:16:26 +0000

    Add emacs playground file

---
+++ emacs.el
@@ -0,0 +1,14 @@
+(helm-find-files-1 (shell-command-to-string "echo -n /home/$USER"))
+
+(defun tomasz-search ()
+  (interactive)
+  (print "search")
+  (helm-do-ag (shell-command-to-string "echo -n /home/$USER")))
+
+(defun tomasz-search-all ()
+  (interactive)
+  (print "search-all")
+  (helm-do-ag (shell-command-to-string "echo -n /")))
+
+(spacemacs/set-leader-keys "saf" 'tomasz-search)
+(spacemacs/set-leader-keys "saf" 'tomasz-search-all)
