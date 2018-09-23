9f5445b4e6a0227f09e8d49a787a3191ef3de705
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2018-09-23 12:22:09 +0100

    Add more comments

--- ocaml-expect-tests-playground/playground.ml
+++ ocaml-expect-tests-playground/playground.ml
@@ -2,10 +2,14 @@ open! Core
 open Async
 
 let with_state f =
-  let state = ref 0 in
-  let%bind result = 
+  let state = ref 1 in
+  let%bind result =
   Monitor.try_with_or_error (fun () ->
-      f (fun () -> incr state; !state)
+        f (fun () ->
+            let x = !state in
+            incr state;
+            incr state;
+            x)
     )
   in
   match result with
@@ -15,20 +19,23 @@ let with_state f =
     Deferred.unit
 
 let%expect_test "test" =
-  let%bind () = 
+  let%bind () =
     with_state (fun gen ->
-        printf "test: %d" (gen ());
+        let num = gen () in
+        printf "test: %d" num;
         let%bind () = [%expect {| test: 1 |}] in
-        if true then failwith "simulate failure";
-        printf "test";
+        let num = gen () in
+        assert (num = 2); (* simulate some complex function that needs num to be 2 and fails *)
+        printf "test: %d" (gen ());
         let%bind () = [%expect {| DID NOT REACH THIS PROGRAM POINT |}] in
         Deferred.unit
       )
   in
+  (* The test failed, print extra internal state to ease debugging. *)
   let%bind () = [%expect {|
-    ((state 1)
+    ((state 5)
      (error
-      (monitor.ml.Error (Failure "simulate failure")
+      (monitor.ml.Error "Assert_failure playground.ml:28:8"
        ("<backtrace elided in test>" "Caught by monitor try_with_or_error"))))|}]
   in
   Deferred.unit
