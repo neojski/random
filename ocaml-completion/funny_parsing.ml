e8551861be1b538a519b804b6d62d26f810600f0
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2018-11-11 16:42:19 +0000

    Add funny parsing example

---
+++ ocaml-completion/funny_parsing.ml
@@ -0,0 +1,60 @@
+open Core
+
+module A = String_id.Make (struct let module_name = "A" end) ()
+module B = String_id.Make (struct let module_name = "B" end) ()
+module C = String_id.Make (struct let module_name = "C" end) ()
+
+module Path = struct
+  type t =
+    | A of A.t
+    | B of A.t * B.t
+    | C of A.t * B.t * C.t
+  [@@deriving sexp]
+
+  let parse x =
+    let aux x f return =
+      match String.lsplit2 ~on:'/' x with
+      | None -> return (f x)
+      | Some (x, rest) -> f x, rest
+    in
+    With_return.with_return (fun { return } ->
+        let (a, rest) = aux x A.of_string (fun a -> return (A a)) in
+        let (b, rest) = aux rest B.of_string (fun b -> return (B (a, b))) in
+        let (c, rest) = aux rest C.of_string (fun c -> return (C (a, b, c))) in
+        ignore (c, rest);
+        assert false
+      )
+
+  let parse x =
+    let aux (type t) (type acc)
+        (of_string : string -> t)
+        (return : (acc * t) -> string * (acc * t))
+        ((str : string), (acc : acc))
+        : string * (acc * t)
+      =
+      match String.lsplit2 ~on:'/' str with
+      | None -> return (acc, of_string str)
+      | Some (x, rest) -> rest, (acc, of_string x)
+    in
+    With_return.with_return (fun { return } ->
+        (x, ())
+        |> aux A.of_string (fun ((), a) -> return (A a))
+        |> aux B.of_string (fun (((), a), b) -> return (B (a, b)))
+        |> aux C.of_string (fun ((((), a), b), c) -> return (C (a, b, c)));
+
+        raise_s [%message "Failed to parse" (x : string)]
+      )
+end
+
+let command =
+  let open Command.Let_syntax in
+  let%map_open () = return ()
+  in
+  fun () ->
+    printf !"%{sexp:Path.t}\n" (Path.parse "a");
+    printf !"%{sexp:Path.t}\n" (Path.parse "a/b");
+    printf !"%{sexp:Path.t}\n" (Path.parse "a/b/c");
+    printf !"%{sexp:Path.t}\n" (Path.parse "a/b/c/d");
+;;
+
+let () = Command.run (Command.basic ~summary:"" command)
